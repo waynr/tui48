a7abf1cc41e30761aca42d88d75547421a54089f
wayne warren <wayne.warren.s@gmail.com>
2023-06-13 16:47:35 -0600

    tui/app: implement initial basic layout for Tui48Board

--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -1,10 +1,11 @@
 use std::io::Write;
 
 use crate::board::{Board, Direction};
+use crate::round::Idx as BoardIdx;
 use crate::error::Result;
 
 mod canvas;
-use canvas::{Bounds2D, Canvas, Idx, Modifier, Rectangle};
+use canvas::{Bounds2D, Canvas, DrawBuffer, Idx, Modifier, Rectangle};
 mod crossterm;
 use crate::tui::crossterm::{next_event, size, Crossterm};
 
@@ -21,41 +22,118 @@ pub(crate) enum UserInput {
     Quit,
 }
 
+struct Tui48BoardDimensions {
+    padding: u16,
+    height: u16,
+    width: u16,
+}
+
+struct Tui48Board {
+    board: DrawBuffer,
+    score: DrawBuffer,
+    slots: Vec<Vec<Option<DrawBuffer>>>,
+}
+
+///
+///  37
+///  ╔══════════════════════════════════╗
+///  ║                                  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║                                  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║                                  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║                                  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║  xxxxxx  xxxxxx  xxxxxx  xxxxxx  ║
+///  ║                                  ║
+///  ╚══════════════════════════════════╝
+///  65
+///  6                                 42
+///
+///
+///
+
+impl Tui48Board {
+    fn new(game: &Board, canvas: &mut Canvas) -> Result<Self> {
+        let mut board = canvas.get_draw_buffer(Rectangle(Idx(5, 5, 0), Bounds2D(36, 25)))?;
+        board.draw_border()?;
+        board.fill(' ')?;
+
+        let mut score = canvas.get_draw_buffer(Rectangle(Idx(18, 1, 0), Bounds2D(10, 3)))?;
+        score.draw_border()?;
+        score.fill(' ')?;
+        score.write(&format!("{}", game.score()))?;
+
+        let (width, height) = game.dimensions();
+        let round = game.current();
+        let mut slots = Vec::with_capacity(height);
+        let x_offset = 5 + 1 + 2;
+        let y_offset = 5 + 1  ;
+        for y in 0..height {
+            let mut row = Vec::with_capacity(width);
+            for x in 0..width {
+                let mut opt = None;
+                let value = round.get(&BoardIdx(x, y));
+                if value > 0 {
+                    let idx = Idx(x_offset + (2 + 6) * x, y_offset + (1 + 5) * y, 5);
+                    let bounds = Bounds2D(6, 5);
+                    let mut card_buffer = canvas.get_draw_buffer(Rectangle(idx, bounds))?;
+                    card_buffer.draw_border()?;
+                    card_buffer.fill(' ')?;
+                    card_buffer.write(&format!("2048"))?;
+                    opt = Some(card_buffer);
+                }
+                row.push(opt);
+            }
+            slots.push(row);
+        }
+        Ok(Self {
+            board,
+            score,
+            slots,
+        })
+    }
+}
+
 pub(crate) struct Tui48 {
     renderer: Box<dyn Renderer>,
     canvas: Canvas,
     board: Board,
+    tui_board: Tui48Board,
 }
 
 impl Tui48 {
     pub(crate) fn new<T: Write + 'static>(board: Board, w: Box<T>) -> Result<Self> {
         let (width, height) = size()?;
+        let mut canvas = Canvas::new(width as usize, height as usize);
+        let tui_board = Tui48Board::new(&board, &mut canvas)?;
         Ok(Self {
             board,
             renderer: Box::new(Crossterm::<T>::new(w)?),
-            canvas: Canvas::new(width as usize, height as usize),
+            canvas,
+            tui_board,
         })
     }
 
     /// Run consumes the Tui48 instance and takes control of the terminal to begin gameplay.
     pub(crate) fn run(mut self) -> Result<()> {
-        let mut buf = self.canvas.get_layer(0)?;
-        buf.modify(Modifier::ForegroundColor(0, 0, 0));
-        buf.modify(Modifier::BackgroundColor(150, 150, 150));
-        let mut small = self
-            .canvas
-            .get_draw_buffer(Rectangle(Idx(2, 5, 2), Bounds2D(20, 6)))?;
-        small.draw_border()?;
-        small.fill(' ')?;
-        let mut overlapping_small = self
-            .canvas
-            .get_draw_buffer(Rectangle(Idx(10, 3, 3), Bounds2D(20, 30)))?;
-        overlapping_small.draw_border()?;
-        overlapping_small.fill('o')?;
         self.renderer.render(&self.canvas)?;
-        //self.initialize_terminal()?;
-        //self.draw_board()?;
-        //self.w.flush()?;
 
         loop {
             match next_event()? {
